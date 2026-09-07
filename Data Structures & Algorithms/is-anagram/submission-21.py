91d4270eaec7c66d1c1b40748e9ac442de3da11a
neerajehh <neerajjrsgp@gmail.com>
2026-09-07 18:49:30 +0800

    Add: is-anagram - submission-21

---
+++ Data Structures & Algorithms/is-anagram/submission-21.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+        for x in s: 
+          s_map[x] = 1+s_map.get(x,0)
+        for x in t: 
+          t_map[x] = 1+t_map.get(x,0)
+        return s_map == t_map
